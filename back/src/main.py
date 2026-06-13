126dca42b4031bf72c2cda68d33afead29172828
Mikhail Zuev <mikhailneue@gmail.com>
2026-06-13 14:07:04 +0300

    del last slash

--- back/src/main.py
+++ back/src/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://147.45.246.64:3000/",
+        "http://147.45.246.64:3000",
     ])
 
 def main():
