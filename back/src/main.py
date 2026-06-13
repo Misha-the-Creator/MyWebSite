dd4fd67d4156ad85e5bd69c4de4ae6c61eadb2e2
Mikhail Zuev <mikhailneue@gmail.com>
2026-06-13 14:03:43 +0300

    hard code frontend origin for now (put it as the env-variable later)

--- back/src/main.py
+++ back/src/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "http://147.45.246.64:3000/",
     ])
 
 def main():
