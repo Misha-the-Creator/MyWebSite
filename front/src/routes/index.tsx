dc45fe4cc99fa7bd7bae47dfab487235581be70d
Mikhail Zuev <mikhailneue@gmail.com>
2026-06-14 13:19:29 +0300

    organizing icons & new homepage title

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -10,32 +10,58 @@ export const Route = createFileRoute("/")({ component: Home });
 function Home() {
   const mapFont = ["i", "v"];
   const headerStr1 = "Mikhail Zuev";
-  // const headerStr2 = "Zue{v}";
 
+  const [theme, setTheme] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light",
+  );
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const handleChange = (e: MediaQueryListEvent) => {
+      setTheme(e.matches ? "dark" : "light");
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, []);
+  console.log(`Поменял тему на ${theme}`);
   return (
-    <div className="font-googlesans text-[20vw] lg:text-[10vw] content-end justify ml-10 -mt-10 -z-2">
-      <p className="">
-        {headerStr1.split("").map((char) =>
-          mapFont.includes(char) ? (
-            <span key={char} className="font-pixel">
-              {char}
-            </span>
-          ) : (
-            char
-          ),
-        )}
-      </p>
-      <p className="-mt-10">
-        {headerStr2.split("").map((char) =>
-          mapFont.includes(char) ? (
-            <span key={char} className="font-pixel">
-              {char}
-            </span>
-          ) : (
-            char
-          ),
-        )}
-      </p>
+    <div className="flex flex-col max-h-screen">
+      <div className="flex justify-center flex-wrap mx-auto my-auto">
+        <div className="w-1/2 md:w-1/4 lg:w-118">
+          <IconOne theme={theme} />
+        </div>
+
+        <div className="w-1/2 md:w-1/4 lg:w-118">
+          <IconTwo theme={theme} />
+        </div>
+
+        <div className="w-1/2 md:w-1/4 lg:w-118">
+          <IconThree theme={theme} />
+        </div>
+
+        <div className="w-1/2 md:w-1/4 lg:w-118">
+          <IconFour theme={theme} />
+        </div>
+      </div>
+      <div className="font-googlesans text-[30vw] lg:text-[16vw] mx-auto mt-auto -z-2">
+        <p className="leading-none overflow-hidden">
+          {headerStr1.split("").map((char, index) =>
+            mapFont.includes(char) ? (
+              <span key={index} className="font-pixel">
+                {char}
+              </span>
+            ) : (
+              char
+            ),
+          )}
+        </p>
+      </div>
     </div>
   );
 }
